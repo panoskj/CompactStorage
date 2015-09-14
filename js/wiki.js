d9644eee5f5c17d3a9c90bebf91b452cad6e856c
tattyseal <toby.strong1@hotmail.co.uk>
2015-09-14 14:59:20 +0100

    Testing wiki system 2

--- js/wiki.js
+++ js/wiki.js
@@ -4,11 +4,14 @@ function getWikiDescriptor()
 	return name;
 }
 
+var item = null;
+
 var csApp = angular.module('CsApp', []);
 
 csApp.controller('CsController', ['$scope', '$http', function($scope, $http) {
 	$http.get("wiki/chest.json").then(function(obj){
 		$scope.item = obj;
+		item = $scope.item;
 		$scope.error = $scope.item == null;
 	});
 }]);
